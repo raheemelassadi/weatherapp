08492021ce61de2622f483e60ca982a641261c58
raheemelassadi <raheemelassadi1@gmail.com>
2023-07-09 18:53:16 -0700

    fetch now working properly....fetcjhing data

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 const API_KEY = 'f14192cf9da2cd1cb88ee683e524190f';
-const CITY_NAME = 'costa mesa';
+const userInput = document.querySelector("#input");
+const locationDisplayName = document.querySelector("#header");
+let cityName = userInput.value;
 
-const url = `http://api.openweathermap.org/data/2.5/forecast?q=${CITY_NAME}&appid=${API_KEY}`;
+userInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault()
+        cityName = event.target.value
+        locationDisplayName.innerHTML = cityName
+        apiCall()
+    }
+})
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
+const apiCall = () => {
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${API_KEY}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.list[0].weather[0].icon)
 
-    console.log(data.list[0].weather[0].icon)
+        })
+        .catch(error => {
+            console.log('Error:', error);
+        });
+}
 
-  })
-  .catch(error => {
-    console.log('Error:', error);
-  });
+
