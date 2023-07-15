6eb2fba12059512bf5b25f35880b97d42ded5305
raheemelassadi <raheemelassadi1@gmail.com>
2023-07-15 02:22:41 -0700

    added graph to weather app

--- main.js
+++ main.js
@@ -44,3 +44,54 @@ const timeCall = () => {
             console.error('Error: ', error);
         });
 }
+
+
+// Sample data for chance of rain (replace with your own data)
+const chanceOfRainData = [
+    { time: '9:00 AM', chance: 20 },
+    { time: '12:00 PM', chance: 40 },
+    { time: '3:00 PM', chance: 60 },
+    { time: '6:00 PM', chance: 30 },
+    { time: '9:00 PM', chance: 10 },
+];
+
+// Get the canvas element and create the chart
+const ctx = document.getElementById('rainChart').getContext('2d');
+const rainChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: chanceOfRainData.map(data => ''), // Empty labels array
+        datasets: [{
+            label: 'Temperature',
+            data: chanceOfRainData.map(data => data.chance),
+            backgroundColor: 'rgba(54, 162, 235, 0.2)', // Blue color below the line
+            borderColor: 'rgba(75, 192, 192, 1)', // Color of the line
+            borderWidth: 2, // Optional
+            fill: true, // Fill below the line
+            pointRadius: chanceOfRainData.map((data, index) => index === 1 ? 3 : 0), // Set point radius to 3 for the first data point
+            tension: .4,
+            pointStyle: 'circle',
+            pointBackgroundColor: 'rgba(75, 192, 192, 1)', // Set point background color
+        }]
+    },
+    options: {
+        responsive: true,
+        scales: {
+            x: {
+                display: false // Hide x-axis labels
+            },
+            y: {
+                display: false // Hide y-axis labels
+            }
+        },        plugins: {
+            legend: {
+                display: false, // Hide legend
+                labels: {
+                    display: false // Hide legend labels
+                }
+            }
+        }
+    }
+});
+
+
