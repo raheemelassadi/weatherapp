f79c3e0504014e747e2531f6508fc55046f6ac98
raheemelassadi <raheemelassadi1@gmail.com>
2023-07-23 04:14:40 -0700

    created dynamic forecast images for upcoming days up to 4 day period

--- main.js
+++ main.js
@@ -6,11 +6,33 @@ const locationDisplayTime = document.querySelector("#location-time");
 let locationHumidity = document.querySelector('#humidity');
 let locationTemp = document.querySelector("#current-weather");
 let locationWind = document.querySelector("#wind-speed-one");
+let dayTwoDate = document.querySelector("#dayTwo")
+let dayThreeDate = document.querySelector("#dayThree")
+let dayFourDate = document.querySelector("#dayFour")
+let description = document.querySelector("#description")
+let dayOneHumidity = document.querySelector("#dayOne-humidity")
+let dayTwoHumidity = document.querySelector("#dayTwo-humidity")
+let dayThreeHumidity = document.querySelector("#dayThree-humidity")
+let dayFourHumidity = document.querySelector("#dayFour-humidity")
+let imgOne = document.querySelector("#img-one")
+let imgTwo = document.querySelector("#img-two")
+let imgThree = document.querySelector("#img-three")
+let imgFour = document.querySelector("#img-four")
 let cityName = userInput.value;
 let dayOne = {};
 let dayTwo = {};
 let dayThree = {};
 let dayFour = {};
+
+
+
+function capitalizeText(str) {
+  return str
+    .toLowerCase()
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
 
 userInput.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
@@ -27,25 +49,42 @@ const weatherCall = () => {
     .then(response => response.json())
     .then(data => {
       locationDisplayName.src = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@4x.png`;
+      let descriptionReturn = data.list[0].weather[0].description
+      description.innerHTML = capitalizeText(descriptionReturn)
 
       //day one temp
       dayOne.temp = ((data.list[0].main.temp - 273.15) * 9 / 5 + 32).toFixed(0);
       locationTemp.innerHTML = dayOne.temp + degSymbol;
-
       //day one humidty
       dayOne.humidity = data.list[0].main.humidity;
       humidity.innerHTML = dayOne.humidity + '%';
-
+      dayOneHumidity.innerHTML = dayOne.humidity + '%'
+      imgOne.src = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@4x.png`
       //day one wind speed
       dayOne.windSpeed = data.list[0].wind.speed;
-      locationWind.innerHTML = dayOne.windSpeed + 'km/h';
+      locationWind.innerHTML = dayOne.windSpeed + 'm/s';
 
 
       dayTwo.temp = ((data.list[1].main.temp - 273.15) * 9 / 5 + 32).toFixed(0)
+      dayTwo.humidity = data.list[1].main.humidity;
+      dayTwoHumidity.innerHTML = dayTwo.humidity + '%'
+      imgTwo.src = `https://openweathermap.org/img/wn/${data.list[1].weather[0].icon}@4x.png`
+
+      
+
 
       dayThree.temp = ((data.list[2].main.temp - 273.15) * 9 / 5 + 32).toFixed(0)
+      dayThree.humidity = data.list[2].main.humidity;
+      dayThreeHumidity.innerHTML = dayThree.humidity + '%'
+      imgThree.src = `https://openweathermap.org/img/wn/${data.list[2].weather[0].icon}@4x.png`
+
 
       dayFour.temp = ((data.list[3].main.temp - 273.15) * 9 / 5 + 32).toFixed(0)
+      dayFour.humidity = data.list[3].main.humidity;
+      dayFourHumidity.innerHTML = dayFour.humidity + '%'
+      imgFour.src = `https://openweathermap.org/img/wn/${data.list[3].weather[0].icon}@4x.png`
+
+
 
       createChart();
     })
@@ -56,30 +95,38 @@ const weatherCall = () => {
 
 //api call to time api
 const timeCall = () => {
-  fetch('https://api.api-ninjas.com/v1/worldtime?city=' + cityName, {
-    method: 'GET',
-    headers: {
-      'X-Api-Key': 'uZc9791oUakQxA28Vs4YJg==JFcpQtrK2dGLUqBn',
-      'Content-Type': 'application/json'
-    }
-  })
-    .then(response => response.json())
-    .then(result => {
-      const dayOfWeek = result.day_of_week.slice(0, 3);
-      locationDisplayTime.innerHTML = `${result.hour}:${result.minute}:${result.second}, ${dayOfWeek}, ${result.month} ${result.day}, ${result.year}`;
+    fetch('https://api.api-ninjas.com/v1/worldtime?city=' + cityName, {
+      method: 'GET',
+      headers: {
+        'X-Api-Key': 'uZc9791oUakQxA28Vs4YJg==JFcpQtrK2dGLUqBn',
+        'Content-Type': 'application/json'
+      }
     })
-    .catch(error => {
-      console.error('Error: ', error);
-    });
-};
+      .then(response => response.json())
+      .then(result => {
+        const dayOfWeek = result.day_of_week.slice(0, 3);
+        let currentDate = new Date(result.year, result.month - 1, result.day);
+        currentDate.setDate(currentDate.getDate() + 1); // Add one day
+        const nextDay = currentDate.getDate();
+        const nextMonth = currentDate.getMonth() + 1;
+        const nextYear = currentDate.getFullYear();
+  
+        dayTwo.date = `${nextMonth}- ${nextDay}`;
+        dayTwoDate.innerHTML = dayTwo.date;
+        locationDisplayTime.innerHTML = `${result.hour}:${result.minute}:${result.second}, ${dayOfWeek}, ${result.month} ${result.day}, ${result.year}`;
+      })
+      .catch(error => {
+        console.error('Error: ', error);
+      });
+  };
 
 
 const createChart = () => {
     const chanceOfRainData = [
       { day: 'Today', chance: parseInt(dayOne.temp) },
       { day: 'Tomorrow', chance: parseInt(dayTwo.temp) },
-      { day: '3rd Day', chance: parseInt(dayThree.temp) },
-      { day: '4th Day', chance: parseInt(dayFour.temp) }
+      { day: 'Day 3', chance: parseInt(dayThree.temp) },
+      { day: 'Day 4', chance: parseInt(dayFour.temp) }
     ];
   
     const temperatureValues = chanceOfRainData.map(data => data.chance);
